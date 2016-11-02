f2d52aa2e7da034baea876bc9cfa60772d06c0b0
Sol1323 <sbeniquez084@eap.edu>
2016-11-02 12:01:01 -0700

    created a data.js file with the JSON

---
+++ scripts/data.js
@@ -0,0 +1,96 @@
+var data = {
+type: "FeatureCollection",
+metadata: {
+generated: 1478110562000,
+url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson",
+title: "USGS Significant Earthquakes, Past Week",
+status: 200,
+api: "1.5.2",
+count: 4
+},
+features: [
+{
+type: "Feature",
+properties: {
+mag: 4.5,
+place: "14km ESE of Pawnee, Oklahoma",
+time: 1478060814200,
+updated: 1478110343116,
+tz: -300,
+url: "http://earthquake.usgs.gov/earthquakes/eventpage/us100073m6",
+detail: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us100073m6.geojson",
+felt: 2949,
+cdi: 5.7,
+mmi: 4.82,
+alert: "green",
+status: "reviewed",
+tsunami: 0,
+sig: 882,
+net: "us",
+code: "100073m6",
+ids: ",us100073m6,",
+sources: ",us,",
+types: ",cap,dyfi,general-link,geoserve,losspager,moment-tensor,origin,phase-data,shakemap,",
+nst: null,
+dmin: 0.136,
+rms: 0.62,
+gap: 65,
+magType: "mwr",
+type: "earthquake",
+title: "M 4.5 - 14km ESE of Pawnee, Oklahoma"
+},
+geometry: {},
+id: "us100073m6"
+},
+{},
+{},
+{
+type: "Feature",
+properties: {
+mag: 6.1,
+place: "3km W of Visso, Italy",
+time: 1477509488320,
+updated: 1478022073766,
+tz: 120,
+url: "http://earthquake.usgs.gov/earthquakes/eventpage/us1000725y",
+detail: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us1000725y.geojson",
+felt: 516,
+cdi: 7.5,
+mmi: 8.55,
+alert: "red",
+status: "reviewed",
+tsunami: 0,
+sig: 2387,
+net: "us",
+code: "1000725y",
+ids: ",us1000725y,gcmt20161026191808,",
+sources: ",us,gcmt,",
+types: ",cap,dyfi,general-link,general-text,geoserve,impact-text,losspager,moment-tensor,origin,phase-data,shakemap,",
+nst: null,
+dmin: 0.249,
+rms: 1.24,
+gap: 21,
+magType: "mww",
+type: "earthquake",
+title: "M 6.1 - 3km W of Visso, Italy"
+},
+geometry: {
+type: "Point",
+coordinates: [
+13.0434,
+42.9343,
+10
+]
+},
+id: "us1000725y"
+}
+],
+bbox: [
+-96.6472,
+3.4372,
+2.56,
+13.0884,
+42.9343,
+44
+]
+}
